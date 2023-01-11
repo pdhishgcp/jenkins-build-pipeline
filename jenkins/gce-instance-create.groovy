c248fe9c3e902709c8ddd950eaa38beadcdfd163
pdhishgcp <121591899+pdhishgcp@users.noreply.github.com>
2023-01-11 13:44:26 +0530

    Update gce-instance-create.groovy

--- jenkins/gce-instance-create.groovy
+++ jenkins/gce-instance-create.groovy
@@ -31,6 +31,8 @@ pipeline {
 	 steps {
     
     sh 'gcloud compute instances create hello-1 --zone=us-central1-a'
+    sh 'gcloud projects add-iam-policy-binding dp2023 --role roles/compute.instances.create --member=serviceAccount:dpterraform@dp2023.iam.gserviceaccount.com'
+
         
     }
     }
